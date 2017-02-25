07a7a13815b18d5bc0c6b9bf87aa8c29931f7d7f
Anand Subramaniam <anand.subra@hotmail.com>
2017-02-25 14:23:52 +0000

    Simplify HTML code + introduce Courier Prime + create print media

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var darkMode = false;
+setLightMode();
+
+function setLightMode(){
+  if (darkMode == false){
+      darkMode = true;
+      $("body").css({'background-color':'#212121', 'color' : 'white'});
+      $(".switch").css("color", "white");
+      $(".script").css("background-color", "#b7b7b7");
+  }
+else  if (darkMode == true){
+      darkMode = false;
+      $("body").css({'background-color': '#eaeaea', 'color' : '#212121'});
+      $(".switch").css("color", "#212121");
+      $(".script").css("background-color", "#f7f7f7");
+  }
+
+}
 
 $(".switch").click(function() {
 if (darkMode == false){
