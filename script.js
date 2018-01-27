b45e041c52369758db3762ae7a78d995729deccb
Anand Subramaniam <anand.subra@hotmail.com>
2018-01-27 20:56:06 +0000

    Fix double scroll bar + logo colour scheme switching

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ setLightMode();
 function setLightMode(){
   if (darkMode == false){
       darkMode = true;
-      $("body").css({'background-color':'#212121', 'color' : 'white'});
+      $("html").css({'background-color':'#212121', 'color' : 'white'});
+      $(".screenplay-logo").css("color", "white");
       $(".switch").css("color", "white");
       $(".print").css("color", "white");
       $(".script").css("background-color", "#b7b7b7");
   }
 else  if (darkMode == true){
       darkMode = false;
-      $("body").css({'background-color': '#eaeaea', 'color' : '#212121'});
+      $("html").css({'background-color': '#eaeaea', 'color' : '#212121'});
+      $(".screenplay-logo").css("color", "#212121");
       $(".switch").css("color", "#212121");
       $(".print").css("color", "#212121");
       $(".script").css("background-color", "#f7f7f7");
@@ -27,14 +29,16 @@ $(".print").click(function() {
 $(".switch").click(function() {
 if (darkMode == false){
     darkMode = true;
-    $("body").css({'background-color':'#212121', 'color' : 'white'});
+    $("html").css({'background-color':'#212121', 'color' : 'white'});
+    $(".screenplay-logo").css("color", "white");
     $(".switch").css("color", "white");
     $(".print").css("color", "white");
     $(".script").css("background-color", "#b7b7b7");
 }
 else if (darkMode == true){
     darkMode = false;
-    $("body").css({'background-color': '#eaeaea', 'color' : '#212121'});
+    $("html").css({'background-color': '#eaeaea', 'color' : '#212121'});
+    $(".screenplay-logo").css("color", "#212121");
     $(".switch").css("color", "#212121");
     $(".print").css("color", "#212121");
     $(".script").css("background-color", "#f7f7f7");
